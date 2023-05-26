6c55d5e24634e9e25476e5a7e91594abf6940b39
Jorge <jorge.toledo.6@hotmail.com>
2023-05-25 20:48:35 -0400

    JS

--- Unidad 2 JavaScript/Clase 25 Mayo/index.js
+++ Unidad 2 JavaScript/Clase 25 Mayo/index.js
@@ -7,7 +7,7 @@ nombre = "Jhon"
 nombre = "Snow"
 
 const btnCambiarTitulo = document.getElementById('btnCambiarTitle') //HTML Element (object)
-
+const h1 = document.getElementById('title')
 
 // btnCambiarTitulo.addEventListener('click' , () => {
 //     let nuevoTitulo = prompt('Ingresa nuevo titulo: ')
@@ -17,7 +17,7 @@ const btnCambiarTitulo = document.getElementById('btnCambiarTitle') //HTML Eleme
 
 const cambiarTitulo2 = () => {
     let nuevoTitulo = prompt('Ingresa nuevo titulo: ')
-    const h1 = document.getElementById('title')
+    
     h1.innerText = nuevoTitulo
 }
 
@@ -26,7 +26,7 @@ btnCambiarTitulo.addEventListener('click', cambiarTitulo2)
 
 function cambiarTitulo() {
     let nuevoTitulo = prompt('Ingresa nuevo titulo: ')
-    const h1 = document.getElementById('title')
+
     h1.innerText = nuevoTitulo
 }
 
@@ -46,6 +46,23 @@ const agregarSubTitulo = () => {
 
 const btnAgregarSubtitulo = document.getElementById('btnAgregarSubtitle')
 btnAgregarSubtitulo.addEventListener('click', agregarSubTitulo)
+
+const btnOcultar = document.getElementById('btnOcultar')
+btnOcultar.addEventListener('click', ()=>{
+    const h1 = document.getElementById('title')
+    h1.style.display = 'none'
+})
+
+const btnToggle = document.getElementById('btnToggleTitulo')
+btnToggle.addEventListener('click', () => {
+    // if(h1.style.display == 'none'){
+    //     h1.style.display = 'block'
+    // }else{
+    //     h1.style.display = 'none'
+    // }
+    //condicion ? valor si true : valor si false
+    h1.style.display == 'block' ? h1.style.display = 'none' : h1.style.display = 'block'
+})
 
 
 
